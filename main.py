159e0e6e34562b0ee7f3947e414c396c9a4b2279
lrtinarelli <lrtinarelli@paradigmadigital.com>
2025-05-22 16:28:10 +0200

    Mejora

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 import os, random
 
+jugador1 = chr(27) + "[1;31m" + "X" + chr(27) + "[;m"
+jugador2 = chr(27) + "[1;32m" + "O" + chr(27) + "[;m"
+registro = []
+
 def imprimir_tablero(tablero):
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -24,9 +28,8 @@ def imprimir_tablero(tablero):
             print("║     ║     ║     ║")
 
 
-def jugar(tablero, posicion):
-
-    tablero = [[ "x" if num == int(posicion) else num for num in fila] for fila in tablero]
+def jugar(tablero, posicion, jugador):
+    tablero = [[ jugador if num == int(posicion) else num for num in fila] for fila in tablero]
 
     imprimir_tablero(tablero)
 
@@ -35,8 +38,15 @@ def jugar(tablero, posicion):
 
 def maquina(tablero, turno):
     if turno == 1:
-        jugar(tablero, "5")
-    
+        return jugar(tablero, "5", jugador1)
+
+    elif turno == 2:
+        if registro[-1] in [2, 4, 6, 8]:
+            return jugar(tablero, random.choice([1, 3, 7, 9]), jugador1)
+        else:
+            return jugar(tablero, random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9]), jugador1)
+    else:
+        return jugar(tablero, random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9]), jugador1)
     #turno 2 de la maquina (turno 3) 
     # -si el jugador va a una arista, poner x en cualquien esquina y gana auto
     # -si el jugador va a una esquina: poner x en 
@@ -50,11 +60,18 @@ def jugador(tablero):
     while verificar_movimieno(tablero, posicion):
         posicion = input("Introduce el número de la posición: ")
 
+        os.system('cls' if os.name == 'nt' else 'clear')
+        imprimir_tablero(tablero)
+        
+        print(chr(27) + "[1;31m" + "[ERROR] - Introduce una posición válida" + chr(27) + "[;m")
+    
+    registro.append(int(posicion))
+    return jugar(tablero, posicion, jugador2)
+
 
 def verificar_movimieno(tablero, posicion):
     for fila in tablero:
         for posiciones in fila:
-            print(posicion, "   ", posiciones)
             if str(posicion) == str(posiciones):
                 return False
     return True
@@ -71,7 +88,7 @@ tablero = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 turno = 1
 
 while True:
-    #tablero = maquina(tablero, turno)
+    tablero = maquina(tablero, turno)
     
     tablero = jugador(tablero)
 
